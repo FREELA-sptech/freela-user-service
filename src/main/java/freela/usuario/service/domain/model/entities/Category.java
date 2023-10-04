f3174ad40b9e7ceaeca7113bfa6eceb72b1530c4
Paulo-Enrique-Santos <paulo.santosfo@sptech.school>
2023-10-04 15:30:59 -0300

    feat: aponta banco aws

--- src/main/java/freela/usuario/service/domain/model/entities/Category.java
+++ src/main/java/freela/usuario/service/domain/model/entities/Category.java
@@ -1,12 +1,14 @@
 package freela.usuario.service.domain.model.entities;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
@@ -17,6 +19,5 @@ public class Category {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Schema(name = "Nome", description = "Nome da categoria", example = "Tatuagem")
     private String name;
 }
--- src/main/java/freela/usuario/service/domain/model/entities/SubCategory.java
+++ src/main/java/freela/usuario/service/domain/model/entities/SubCategory.java
@@ -1,6 +1,5 @@
 package freela.usuario.service.domain.model.entities;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +16,7 @@ public class SubCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Schema(name = "Nome", description = "Nome da subCategoria", example = "Tatuagem realista")
     private String name;
-
     @ManyToOne
     private Category category;
 }
